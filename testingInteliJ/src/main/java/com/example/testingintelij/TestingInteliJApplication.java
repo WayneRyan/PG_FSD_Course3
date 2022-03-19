36598a3fd1aa2c746f65c607449d0ac6edce38d8
Wayne J. Ryan <wryan@smithdrug.com>
2022-03-19 09:41:00 -0400

    initiating project

---
+++ testingInteliJ/src/main/java/com/example/testingintelij/TestingInteliJApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingintelij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingInteliJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingInteliJApplication.class, args);
+    }
+
+}
