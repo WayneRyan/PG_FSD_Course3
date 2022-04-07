8e6922ac98a3d99eaaf5b72053fe2d621e87a792
Wayne J. Ryan <wryan@smithdrug.com>
2022-04-07 07:34:12 -0400

    Connecting to the database

--- FlyAway/src/test/java/Main.java
+++ FlyAway/src/test/java/Main.java
@@ -1,5 +1,29 @@
+import entity.PlacesEntity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        try {
+            et.begin();
+            PlacesEntity pl = new PlacesEntity();
+            pl.setId(1);
+            pl.setName("Chicago");
+            em.persist(pl);
+            et.commit();
+        } finally {
+            if (et.isActive()) {
+                et.rollback();
+            }
+            em.close();
+            emf.close();
+        }
 
     }
 }
