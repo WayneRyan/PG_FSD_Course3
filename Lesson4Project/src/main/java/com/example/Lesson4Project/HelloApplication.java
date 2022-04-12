2252361da86f53db8d1dc93d29c6dd8d6a82a94f
Wayne J. Ryan <wryan@smithdrug.com>
2022-04-12 08:55:53 -0400

    Setting up project for 4th lecture assignment

---
+++ Lesson4Project/src/main/java/com/example/Lesson4Project/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.Lesson4Project;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
